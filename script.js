0e7b4b9fb79e8e02ac4ce836d9c9ce159a55c5b4
unknown <lauraocampo353@gmail.com>
2024-03-15 16:59:32 -0400

    coment

---
+++ script.js
@@ -0,0 +1,76 @@
+
+const lista = [
+    "cebolla",
+    "aceite maseite",
+    "flips",
+    "cafe",
+    "pimenton",
+    "tomate"
+
+
+
+
+]
+
+const usuario = {
+    name: "laura",
+    age: "16",
+    job: "fronted develop",
+
+}
+
+
+
+
+console.log([usuario, usuario, usuario])
+
+
+
+
+
+
+
+
+
+// un String o cadena de texto se define como un conjunto de caracteres y para definirlos haremos uso de comillas dobles, simples o backtick
+// el tipo de dato numerico nos permiten guardar numeros sean enteros o decimales
+
+// solo tiene dos tipos de valores verdadero y falso
+// el cero funciona como si fuera false y el uno como true
+
+// un Array es una lista de datos ordenados en memoria uno detras de otro a los cuales se les asigna un indice cuyo primer indice es cero el ultimo indice es -1
+
+// un objeto es un tipo de dato completo que me permite almacenar los datos por clave y valor
+
+// undefined : es un tipo de dato indefinido que espera obtener un valor por lo general este dato lo obtenemos cuando algo sale mal
+// null: tipo de dato vacio y esta vacio intencionalemnte
+let variable = 1
+
+    // **
+    // *
+    // *+ suma
+    // *- resta
+    // */ divide
+    // *% resto
+    // *
+    // *== esto es igual
+    // *=== esto es exactamente igual a
+    // *!= esto es diferente
+    // *!== esto es exactamente diferente a
+    // *
+    // *&& and
+    // *
+    // *// or
+    // *
+    *
+
+
+
+
+
+if (variable) {
+    console.log("se cumplio")
+
+} else {
+    console.log("no se cumplio")
+}
